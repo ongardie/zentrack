04cdc915e057c2e9f2e0627acfaf5eb4eeca53af
phpzen <phpzen>
2002-03-27 06:34:03 +0000

    form for adding projects

---
+++ www/addProjectSubmit.php
@@ -0,0 +1,83 @@
+<?
+  /*
+  **  NEW PROJECT (add submit)
+  **  
+  **  Commit new project to database
+  **
+  */
+  
+  
+  include("header.php");
+
+  $page_tile = "Commit New Project";
+  $expand_projects = 1;
+
+  // initiate default values
+  $otime = time();  // set time ticket opened
+  if( $deadline ) {
+     $deadline = $zen->dateParse($deadline);
+  }
+  if( $start_date ) {
+     $start_date = $zen->dateParse($start_date);
+  }
+
+  $typeID = $zen->projectTypeID();
+  $description = nl2br(htmlentities($description));
+
+  $fields = array(
+		  "title"       => "text",
+		  "priority"    => "int",
+		  "description" => "ignore",
+		  "otime"       => "int",
+		  "binID"       => "int",
+		  "typeID"      => "int",
+		  "userID"      => "int",
+		  "systemID"    => "int",
+		  "tested"      => "int",
+		  "approved"    => "int",
+		  "relations"   => "text",
+		  "projectID"   => "int",
+		  "est_hours"   => "num",
+		  "deadline"    => "int",
+		  "start_date"  => "int"
+		  );
+ $required = array(
+		   "title",
+		   "priority",
+		   "description",
+		   "binID",
+		   "typeID",
+		   "systemID"
+		   );
+  $zen->cleanInput($fields);
+  // check for required fields
+  foreach($required as $r) {
+     if( !$$r ) {
+	$errs[] = ucfirst($r)." is a required field";
+     }
+  }
+  if( !$errs ) {
+     // create an array of existing fields
+     foreach(array_keys($fields) as $f) {
+	if( strlen($$f) ) {
+	   $params["$f"] = $$f;
+	}
+     }
+     $params["creatorID"] = $login_id;
+     // add the ticket to db
+     $id = $zen->add_ticket($params);
+     // check for errors
+     if( !$id ) {
+	$errs[] = "Could not create ticket. ".$zen->db_error;
+     }
+  }
+
+  if( !$errs ) {
+     header("Location:$rootUrl/ticket.php?id=$id");
+  } else {
+     include("$libDir/nav.php");
+     $zen->print_errors($errs);
+     include("$templateDir/newTicketForm.php");
+     include("$libDir/footer.php");
+  }
+?>
---
+++ www/newProject.php
@@ -0,0 +1,19 @@
+<?
+  /*
+  **  NEW PROJECT
+  **  
+  **  Create a new project
+  **
+  */
+  
+  
+  include("header.php");
+
+  $page_tile = "Create a New Project";
+  $expand_projects = 1;
+  include("$libDir/nav.php");
+
+  include("$templateDir/newProjectForm.php");
+
+  include("$libDir/footer.php");
+?>
