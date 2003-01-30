41db1255eef8937d1f574894ae55ce1389ff5f31
phpzen <phpzen>
2003-01-30 17:50:22 +0000

    The tooltips fixed in list ticket views Thanks to Jamin W. Collins (jamincollins) for this fix.

--- includes/templates/listTickets.php
+++ includes/templates/listTickets.php
@@ -7,28 +7,28 @@ if( is_array($tickets) ) {
       ?>
         <table width="100%" cellspacing='1' cellpadding='2' bgcolor='<?=$zen->settings["color_alt_background"]?>'>
 	<tr bgcolor="<?=$zen->settings["color_title_background"]?>">
-	<td width="32" height="25" valign="middle" title="Tracking ID for the $page_type">
+	<td width="32" height="25" valign="middle" title="Tracking ID for the <?=$page_type?>">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("ID")?></span></b></span></div>
 	</td>
-	<td height="25" valign="middle" title="The name of the $page_type">
+	<td height="25" valign="middle" title="The name of the <?=$page_type?>">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Title")?></span></b></span></div>
 	</td>
-	<td width="32" height="25" valign="middle" title="The importance of the $page_type">
+	<td width="32" height="25" valign="middle" title="The importance of the <?=$page_type?>">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Pri")?></span></b></span></div>
 	</td>
 	<td width="32" height="25" valign="middle" title="The type of task to complete">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Type")?></span></b></span></div>
 	</td>
-	<td width="60" height="25" valign="middle" title="When the $page_type was created">
+	<td width="60" height="25" valign="middle" title="When the <?=$page_type?> was created">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Opened")?></span></b></span></div>
 	</td>
-	<td width="40" height="25" valign="middle" title="Who the $page_type belongs to">
+	<td width="40" height="25" valign="middle" title="Who the <?=$page_type?> belongs to">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Owner")?></span></b></span></div>
 	</td>
-	<td width="60" height="25" valign="middle" title="The location of the $page_type">
+	<td width="60" height="25" valign="middle" title="The location of the <?=$page_type?>">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Bin")?></span></b></span></div>
 	</td>
-	<td width="80" height="25" valign="middle" title="The length of time the $page_type has been open">
+	<td width="80" height="25" valign="middle" title="The length of time the <?=$page_type?> has been open">
 	<div align="center"><span style="color:<?=$zen->settings["color_title_txt"]?>"><b><span class="small"><?=$zen->prn("Time")?></span></b></span></div>
 	</td>
 	</tr>
