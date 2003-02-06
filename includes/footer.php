b6c055a509254ecd80b81ae51b3d9e8835fe7d7a
phpzen <phpzen>
2003-02-06 18:08:21 +0000

    updated debug information

--- includes/footer.php
+++ includes/footer.php
@@ -30,21 +30,22 @@
   **  This is the debugging section... please keep this intact, as
   **  we use it extensively for support questions
   */
-  if( $zen->debug > 0 ) {
+  if( strlen($zen->debug) ) {
      print "<span color='#666666'>\n";
      print "<p>&nbsp;------DEBUG OVERVIEW-------&nbsp;</p>\n";
      print "<a href='$rootUrl/phpinfo.php'>click here to view phpinfo</a><br>\n";
-     print "USER: $HTTP_USER_AGENT<br>\n";
-     print "SCRIPT: $SCRIPT_NAME<br>\n";
-     print "URL: $HTTP_HOST<br>\n";
+     print "HTTP_USER_AGENT: $HTTP_USER_AGENT<br>\n";
+     print "SCRIPT_NAME: $SCRIPT_NAME<br>\n";
+     print "HTTP_HOST: $HTTP_HOST<br>\n";
      print "DOCROOT: $DOCUMENT_ROOT<br>\n";
-     print "COOKIE: $HTTP_COOKIE<br>\n";
-     print "SERVER: {$_SERVER['SERVER_SOFTWARE']}<br>\n";
+     print "HTTP_COOKIE: $HTTP_COOKIE<br>\n";
+     print "SERVER_SOFTWARE: {$_SERVER['SERVER_SOFTWARE']}<br>\n";
      print "SERVER_NAME: $SERVER_NAME<br>\n";
      print "rootUrl: $rootUrl<br>\n";
-     print "database: ".$zen->database_type."/".$zen->database_host."<br>\n";
-     print "<p>Attachments: ".$zen->settings["attachment_types_allowed"]."</p>\n";
      print "page_browser: $page_browser<br>\n";
+     print "zenTrack Version: ".$zen->settings["version_xx"]."<br>\n";
+     print "database: ".$zen->database_type."/".$zen->database_host."<br>\n";
+     print "Attachments types: ".$zen->settings["attachment_types_allowed"]."<br>\n";
      print "databaseConnection: ".$zen->db_link."<br>\n";
      print "settings count: ".count($zen->settings)."<br>\n";
      print "bins: ".join(",",$zen->bins)."<br>\n";
@@ -52,14 +53,14 @@
      print "priorities: ".join(",",$zen->priorities)."<br>\n";
      print "systems: ".join(",",$zen->systems)."<br>\n";
      if( $login_id ) {
-			print "login_id: $login_id<br>\n";
-			print "login_level: $login_level<br>\n";
-			print "login_name: $login_name<br>\n";
-			print "login_bin: $login_bin<br>\n";     
-			print "userBins: ".join(",",$zen->getUsersBins($login_id))."<br>\n";
+       print "login_id: $login_id<br>\n";
+       print "login_level: $login_level<br>\n";
+       print "login_name: $login_name<br>\n";
+       print "login_bin: $login_bin<br>\n";     
+       print "userBins: ".join(",",$zen->getUsersBins($login_id))."<br>\n";
      } else {
-			print "Not logged in<br>\n";
-		 }
+       print "Not logged in<br>\n";
+     }
      print "<p>&nbsp;------/DEBUG OVERVIEW-------&nbsp;</p>\n";
      print "</span>\n";
      $zen->printDebugMessages();
