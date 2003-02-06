73b742993ffc7e3906d850c415436a4994bd4304
phpzen <phpzen>
2003-02-06 18:25:32 +0000

    fixed error message printout

--- www/actions/upload.php
+++ www/actions/upload.php
@@ -42,10 +42,28 @@
 	   $errs[] = " $r is required";
 	}
      }
+     // print out an appropriate error message
+     // thanks to Jeremy G. for this correction
      if( $userfile_error ) {
-       $errs[] = $userfile_error;
-     }
-     
+       switch ($userfile_error){
+       case 1:
+	 $errs[] = "The uploaded file exceeds the upload_max_filesize directive in php.ini.";
+	 break;
+       case 2:
+	 $errs[] = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the html form.";
+	 break;
+       case 3:
+	 $errs[] = "The uploaded file was only partially uploaded.";
+	 break;
+       case 4:
+	 $errs[] = "No file was uploaded.";
+	 break;
+       case 5:
+	 $errs[] = "Uploaded file size 0 bytes";
+	 break;
+       }
+     } 
+     // if there aren't any errors, get the file and input to the system
      if( !$errs ) {
 	// perform the file transfer to move it to the directory where we 
 	// want to keep it
@@ -67,11 +85,15 @@
 	}
         $max_size = $zen->settings["attachment_max_size"];	
 	if( !is_uploaded_file($userfile) ) {
-	   $errs[] = "The file was not recieved.  Check to insure it's size does not exceed ".substr($max_size,0,strlen($max_size)-3)."KB";
+	  $errs[] = "The file was not recieved.  "
+	    ."Check to insure it's size does not exceed ".substr($max_size,0,strlen($max_size)-3)."KB";
 	} else if( $userfile_size > $max_size ) {
-	   $errs[] = "The file size (".number_format($userfile_size).") exceeded the maximum allowed (".number_format($max_size).")";
-	} else if( $zen->settings["attachment_types_allowed"] && !preg_match("/\b$file_type\b/i", $zen->settings["attachment_types_allowed"]) ) {
-	   $errs[] = "'$file_type' is not an allowed file extension - See your systems administrator for more information";
+	   $errs[] = "The file size (".number_format($userfile_size)
+	     .") exceeded the maximum allowed (".number_format($max_size).")";
+	} else if( $zen->settings["attachment_types_allowed"] 
+		   && !preg_match("/\b$file_type\b/i", $zen->settings["attachment_types_allowed"]) ) {
+	   $errs[] = "'$file_type' is not an allowed file extension - "
+	     ."See your systems administrator for more information";
 	} else if( !$userfile_type ) {
 	   $errs[] = "That is not a recognized file type, by your browsers determinations";	   
 	} else if( !move_uploaded_file($userfile, $zen->attachmentsDir."/$file_name") ) {
@@ -79,42 +101,43 @@
 	}
 	
 	if( !$errs ) {
-	   @chmod($zen->attachmentsDir."/$file_name",0666);
-	   $params = array(
-			   "name"     => $userfile_name,
-			   "filename" => $file_name,
-			   "filetype" => $userfile_type
-			   );
-	   if( $comments )
-	     $params["description"] = $comments;
-	   $res = $zen->attach_to_ticket( $ticket_id, $login_id, $params, $log_id);
-	   if( $res ) {
-	      add_system_messages("Attachment $userfile_name uploaded for ticket $id");
-	      $setmode = "attachments";
-	      include("../ticket.php");
-	      exit;
-	   } else {
-	      $errs[] = "System error: Attachment $userfile_name could not be uploaded for ticket $id. ".$zen->db_error;
-	   }
+	  @chmod($zen->attachmentsDir."/$file_name",0666);
+	  $params = array(
+			  "name"     => $userfile_name,
+			  "filename" => $file_name,
+			  "filetype" => $userfile_type
+			  );
+	  if( $comments )
+	    $params["description"] = $comments;
+	  $res = $zen->attach_to_ticket( $ticket_id, $login_id, $params, $log_id);
+	  if( $res ) {
+	    add_system_messages("Attachment $userfile_name uploaded for ticket $id");
+	    $setmode = "attachments";
+	    include("../ticket.php");
+	    exit;
+	  } else {
+	    $errs[] = "System error: Attachment $userfile_name could not be uploaded for ticket $id. "
+	      .$zen->db_error;
+	  }
 	}
      }
      if( $errs )
        add_system_messages( $errs, 'Error' );     
   }
-
+  
   include("$libDir/nav.php");
-
+  
   if( $actionComplete == 1 ) {
-     $id = $ticket_id;
+    $id = $ticket_id;
      $ticket = $zen->get_ticket($id);     
   }
   extract($ticket);
   if( strtolower($zen->types["$type_id"]) == "project" ) {
-     include("$templateDir/projectView.php");
+    include("$templateDir/projectView.php");
   } else {
-     include("$templateDir/ticketView.php");     
+    include("$templateDir/ticketView.php");     
   }
-
+  
   include("$libDir/footer.php");
-
+  
 }?>
