91c3b5752a1c8ec917302abcde5923b7426fe76c
altman <altman>
2003-02-14 23:26:41 +0000

    Schema Archiving

---
+++ install/UPGRADE_SEED_oracle.sql
@@ -0,0 +1,17 @@
+INSERT INTO ZENTRACK_SETTINGS 
+(name, value, description) 
+VALUES 
+('default_notify_manager','on','Add bin manager to notify list by default.');
+
+
+INSERT INTO ZENTRACK_SETTINGS
+(name, value, description)
+VALUES
+('default_notify_tester','on','Add bin tester to notify list by default.');
+
+
+INSERT INTO ZENTRACK_SETTINGS
+(name, value, description)
+VALUES
+('default_notify_creator','on','Add ticket creator to notify list by default.');
+
