539c962ef1b85392b9609de6026da97126677c6d
phpzen <phpzen>
2003-02-20 07:52:37 +0000

    removed drop stuff that isn't needed

--- install/build_postgres.sql
+++ install/build_postgres.sql
@@ -5,44 +5,6 @@
 -- 05/03/02
 --
 
---DROP SEQUENCE "access_access_id_seq";
---DROP SEQUENCE "attachments_attachment_id_seq";
---DROP SEQUENCE "bins_bid_seq";
---DROP SEQUENCE "logs_lid_seq";
---DROP SEQUENCE "priorities_pid_seq";
---DROP SEQUENCE "settings_setting_id_seq";
---DROP SEQUENCE "systems_sid_seq";
---DROP SEQUENCE "tasks_task_id_seq";
---DROP SEQUENCE "tickets_id_seq";
---DROP SEQUENCE "translation_strings_trans_id_seq";
---DROP SEQUENCE "translation_words_word_id_seq";
---DROP SEQUENCE "types_type_id_seq";
---DROP SEQUENCE "users_user_id_seq";
---DROP SEQUENCE "reports_id_seq";
---DROP SEQUENCE "reports_temp_id_seq";
---DROP SEQUENCE "notify_id_seq";
-
---DROP TABLE ZENTRACK_ACCESS;
---DROP TABLE ZENTRACK_ATTACHMENTS;
---DROP TABLE ZENTRACK_BINS;
---DROP TABLE ZENTRACK_LOGS;
---DROP TABLE ZENTRACK_LOGS_ARCHIVED;
---DROP TABLE ZENTRACK_PREFERENCES;
---DROP TABLE ZENTRACK_PRIORITIES;
---DROP TABLE ZENTRACK_SETTINGS;
---DROP TABLE ZENTRACK_SYSTEMS;
---DROP TABLE ZENTRACK_TASKS;
---DROP TABLE ZENTRACK_TICKETS;
---DROP TABLE ZENTRACK_TICKETS_ARCHIVED;
---DROP TABLE ZENTRACK_TRANSLATION_STRINGS;
---DROP TABLE ZENTRACK_TRANSLATION_WORDS;
---DROP TABLE ZENTRACK_TYPES;
---DROP TABLE ZENTRACK_USERS;
---DROP TABLE ZENTRACK_REPORTS;
---DROP TABLE ZENTRACK_REPORTS_TEMP;
---DROP TABLE ZENTRACK_REPORTS_INDEX;
---DROP TABLE ZENTRACK_NOTIFY_LIST;
-
 CREATE SEQUENCE "access_access_id_seq" start 1001 increment 1 maxvalue 2147483647 minvalue 1  cache 1;
 CREATE SEQUENCE "attachments_attachment_id_seq" start 1001 increment 1 maxvalue 2147483647 minvalue 1  cache 1;
 CREATE SEQUENCE "bins_bid_seq" start 1001 increment 1 maxvalue 2147483647 minvalue 1  cache 1;
