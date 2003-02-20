95137d7b31651981f9cd1df667bfa65217a0d455
phpzen <phpzen>
2003-02-20 07:56:17 +0000

    committing new template files for egate system

---
+++ includes/templates/email/email_options.php
@@ -0,0 +1,10 @@
+
+The following options are available for ticket <?=id?>, at present:
+
+<?
+ foreach($actions as $a) {
+    print ucwords($a)."\n";
+ }
+?>
+
+For more information on using these options, reply to this message with the word "help" in the subject.
---
+++ includes/templates/email/email_test.php
@@ -0,0 +1,9 @@
+
+This ticket is ready for testing.  You may mark this ticket as tested simply by
+replying to this email with "#<?=$id?> test" in the subject.  
+
+This is normally accomplished simply by hitting reply.
+
+You may enter hours worked by putting "hours: nnn" in the body, where nnn is the number of hours worked.
+
+You may also place any comments or useful information in the body of the reply.
