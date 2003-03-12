9f12e0ac55e13e842e056093e414b5ebb66bbaaa
phpzen <phpzen>
2003-03-12 13:23:27 +0000

    added setup_from_cvs for windows users

---
+++ setup_from_cvs.cmd
@@ -0,0 +1,8 @@
+
+mkdir includes\attachments
+mkdir includes\logs
+mkdir includes\cache
+
+copy install\configVars.php.default includes\configVars.php
+copy install\header.php.default www\header.php
+copy install\egate_config.php.default includes\egate_config.php
