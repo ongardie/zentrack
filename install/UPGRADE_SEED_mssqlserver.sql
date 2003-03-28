0b81bd401c2fe27251c48fc17145da8271095374
phpzen <phpzen>
2003-03-28 10:04:19 +0000

    implementation of language preference and default language properties

---
+++ install/UPGRADE_SEED_mssqlserver.sql
@@ -0,0 +1,4 @@
+
+UPDATE ZENTRACK_SETTINGS SET description = 'This must match one of the file names in the translations folder' WHERE setting_id = 79;
+
+UPDATE ZENTRACK_SETTINGS SET value = '2.4' WHERE name = 'version_xx';
