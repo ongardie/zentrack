a8fe2950cb35966e4a80302ff7e25dbd5f9cbc6a
phpzen <phpzen>
2003-04-22 03:06:31 +0000

    fixed link translation error

--- www/admin/editUserSubmit.php
+++ www/admin/editUserSubmit.php
@@ -55,9 +55,10 @@
       if( $res ) {
 	$msg = tr("User ? was updated successfully. ? to customize user ?'s access permissions.",
 		  array($user_id,
-			"<br><a href='$rootUrl/admin/access.php?user_id=$user_id'>"
+			"--link--",
 			.tr("Click Here")."</a>",
 			$user_id));
+	$msg = str_replace("--link--","<br><a href='$rootUrl/admin/access.php?user_id=$user_id'>");
       } else {
 	$errs[] = tr("System Error: Could not update ?, ?", array($lname, $fname));
       }
