e6013dbe51680846ab9c5801493e1b75c9357fdd
rjch <rjch>
2003-05-03 13:33:20 +0000

    Fixed minor "oops" in update SQL statement.

--- install/UPGRADE_SEED_oracle.sql
+++ install/UPGRADE_SEED_oracle.sql
@@ -1,5 +1,5 @@
 
 UPDATE ZENTRACK_SETTINGS SET description = 'This must match one of the file names in the translations folder' WHERE setting_id = 79;
-UPDATE ZENTRACK_SETTINGS SET description = 'Send email to tester/approver when ticket is pending' WHEN setting_id = 27;
+UPDATE ZENTRACK_SETTINGS SET description = 'Send email to tester/approver when ticket is pending' WHERE setting_id = 27;
 
 UPDATE ZENTRACK_SETTINGS SET value = '2.4.1' WHERE name = 'version_xx';
