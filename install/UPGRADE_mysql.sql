d549ff42a330483559730015eaa44b9f149a551c
phpzen <phpzen>
2003-05-08 07:08:13 +0000

    updating seed files

--- install/UPGRADE_mysql.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE ZENTRACK_PREFERENCES;
-
-CREATE TABLE ZENTRACK_PREFERENCES (
-  user_id  int(12) NOT NULL default '0',
-  prefname varchar(25),
-  prefval  varchar(50),
-  index (user_id)
-) TYPE=MyISAM;
--- install/UPGRADE_postgres.sql
+++ install/UPGRADE_postgres.sql
@@ -1,9 +1,1 @@
 
-DROP TABLE ZENTRACK_PREFERENCES;
-
-CREATE TABLE ZENTRACK_PREFERENCES (
-  user_id int8 default '0' NOT NULL,
-  prefname varchar(25) default NULL,
-  prefval  varchar(50) default NULL
-);
-
