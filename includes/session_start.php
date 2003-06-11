07b8c7e10e419303c4a218dfe8c0d3b33e72dd79
phpzen <phpzen>
2003-06-11 16:57:44 +0000

    fix for security, added some new entries

--- includes/session_start.php
+++ includes/session_start.php
@@ -16,8 +16,11 @@ session_start();
 // ... except the following list
 $reservedList = array("libDir", "rootUrl", 
 		      "Db_Type", "Db_Instance", "Db_Login", "Db_Pass", "Db_Host", 
-		      "Debug_Mode", "Demo_Mode", "page_prefix", "page_title", 
-		      "configFile", "system_message_limit", "login_id");
+		      "Debug_Mode", "Demo_Mode", 
+		      "page_prefix", "page_title", "page_section", 
+		      "configFile", "system_message_limit", 
+		      "login_id", "login_level", "login_inits", "login_bin", 
+		      "login_language", "login_name");
 
 // dump the system variables for use
 if( isset($_SERVER) ) {
