c83a381bc9868deaa3436a9735c737634b334f44
mjlivelyjr <mjlivelyjr>
2004-03-26 16:19:55 +0000

    Began implementation of the class

--- includes/lib/classes/ZenEmail.php
+++ includes/lib/classes/ZenEmail.php
@@ -37,13 +37,27 @@ class ZenEmail extends Zen {
    */
   function setRecipients( $recipients ) { 
     if( is_object($recipients) && is_a("ZenUserList") ) {
-      //todo
+      //reset the list and begin to loop through it
+      $recipients->reset();
+      while ($recipients->hasNext()) {
+      	$zenUser = $recipients->next();
+      	//TODO: add the email of the user to the recipients array
+      	//$this->_recipients[] = $zenUser->_email;
+      }
     }
     if( is_object($recipients) && is_a("ZenNotifyList") ) {
-      //todo
+      //reset the list and begin to loop through it
+      $recipients->reset();
+      while ($recipients->hasNext()) {
+      	$zenNotify = $recipients->next();
+      	//TODO: I have no idea what type of object is going to be loaded into zenNotify. 
+      	//Need to find out and implement accordingly
+      }
     }
     else if( is_array($recipients) ) {
-      //todo
+      //direct array copy is all that's needed
+      $this->_recipients = $recipients;
+      return true;
     }
     else { return false; }
   }
@@ -55,7 +69,11 @@ class ZenEmail extends Zen {
    * @param array $vals are the values to pass to the template
    * @return boolean template found and loaded successfully
    */
-  function addTemplate( $template, $vals = null ) { }
+  function addTemplate( $template, $vals = null ) {
+  	$this->_templateVars = $vals;
+  	$this->_template = new ZenTemplate(); //TODO: figure out how to initialize a template
+  	return true;
+  }
 
   /**
    * Adds additional headers in the (unlikely) event that a special header is desired
@@ -72,7 +90,12 @@ class ZenEmail extends Zen {
    * 
    * @return boolean message sent successfully
    */
-  function send() { }
+  function send() {
+  	$recipients = implode(', ', $this->_recipients);
+  	$message = $this->_template->fetch(); //TODO: This function needs to be fixed
+  	$headers = implode("\n", array_merge($this->_headers, array("From: {$this->_sender}")));
+  	mail($recipients, $this->_subject, $message, $headers);
+  }
 
   /**
    * formats text for use in email with proper escape characters
@@ -82,7 +105,13 @@ class ZenEmail extends Zen {
    * @param string $text to format
    * @return string $text formatted
    */
-  function formatForEmail( $text ) { }
+  function formatForEmail( $text ) {
+  	if (get_magic_quotes_gpc()) {
+  		return stripslashes($text);
+  	} else {
+  		return $text;
+  	}
+  }
 
 
   /** @var array $_recipients the unique list of recipients */
@@ -96,6 +125,12 @@ class ZenEmail extends Zen {
 
   /** @var string $_message the message to send */
   var $_message;
+  
+  /** @var string $_template the template to use for the message */
+  var $_template;
+  
+  /** @var array $_templateVars The variables (besides 'msg') to use with the template */
+  var $_templateVars;
 
   /** @var array $_headers headers to send with message */
   var $_headers = array();
