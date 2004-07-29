f1808aa87e5328e75f8879cb5bee22f7850d1c88
cfalonso <cfalonso>
2004-07-29 19:22:02 +0000

    Working behaviors admin panel

---
+++ includes/templates/behaviorDetailForm.php
@@ -0,0 +1,108 @@
+      <br>
+      <p class='error'><?=tr("This screen will let you list, add, edit and delete behavior rules.") . '  '
+          .tr("Please refer to documentation.")?></p>
+      <ul>
+      <form name='behaviorDetailForm' action='<?=$SCRIPT_NAME?>' method='post'>
+      <input type='hidden' name='TODO' value=''>
+      <input type='hidden' name='behavior_id' value='<?=$behavior_id?>'>
+      <table cellpadding="2" cellspacing="1" class='plainCell'>
+	 <tr>
+	 <td class='titleCell' align='center' colspan='4'>
+	   <b>Edit the Behavior Rules</b>
+	 </td>
+	 </tr>
+	 <tr>
+	 <td class='cell' align='center'><b><?=tr("Compare Field")?></b></td>
+	 <td class='cell' align='center'><b><?=tr("Operator")?></b></td>
+	 <td class='cell' align='center'><b><?=tr("Compare Value")?></b></td>
+	 <td class='cell' align='center'><b><?=tr("Sort Order")?></b></td>
+	 </tr>
+    <? 
+         $num = count($elements) + $more;
+	 if( is_array($elements) ) {
+	   $j = 0;
+	   $t = "\t<td class='bars'>";
+	   $te = "</td>\n";
+           foreach ($elements as $item) {
+             if (isset($item['field_name'])) {
+	       print "<tr>\n";
+               print "$t<select name='NewFieldName[".$j."]'>\n";
+               print "<option value=''>".tr('-new/delete-')."</option>\n";
+               foreach($field_list as $fl=>$fn) {
+                 $sel=($item['field_name']==$fn)? " selected" : "";
+                 print "<option value='$fn'$sel>$fl</option>\n";
+               }
+               print "$te";
+               print "$t<select name='NewComparator[".$j."]'>\n";
+               foreach($comp_opers as $cl=>$cn) {
+                 $sel=($item['comparator']==$cn)? " selected" : "";
+                 print "<option value='$cn'$sel>$cl</option>\n";
+               }
+               print "$te";
+               print "$t"."<input type='text' name='NewMatchValue[".$j."]' value='";
+               print $item['match_value'];
+               print "' size='20' maxlength='255'>"."$te";
+               print "$t"."<input type='text' name='NewSortOrder[".$j."]' value='".$item['sort_order'];
+               print "' size='3' maxlength='3'>"."$te";
+	       print "</tr>\n";
+	       $j++;
+             }
+	   }
+	 }
+	 for( $i=0; $i<$more; $i++ ) {
+	   print "<tr>\n";
+           print "$t<select name='NewFieldName[".$j."]'>\n";
+           print "<option value='' selected>".tr('-new/delete-')."</option>\n";
+           foreach($field_list as $fl=>$fn) {
+             print "<option value='$fn'>$fl</option>\n";
+           }
+           print "$te";
+           print "$t<select name='NewComparator[".$j."]'>\n";
+           foreach($comp_opers as $cl=>$cn) {
+             $sel=($cn=='equals')? " selected" : "";
+             print "<option value='$cn'$sel>$cl</option>\n";
+           }
+           print "$te";
+           print "$t"."<input type='text' name='NewMatchValue[".$j."]' value='";
+           print $item['match_value'];
+           print "' size='20' maxlength='255'>"."$te";
+           print "$t"."<input type='text' name='NewSortOrder[".$j."]' value='".$item['sort_order'];
+           print "' size='3' maxlength='3'>"."$te";
+	   print "</tr>\n";
+	   $j++;
+	 }
+
+    ?>
+<tr>
+  <td class="titleCell" colspan="4">
+    <?=tr('Press MORE to create new detail items')?>
+    <br>
+    <?=tr('Press LESS to remove blank rows')?>
+    <br>
+    <?=tr('Press Save to save changes')?>
+    <br>
+    <?=tr('Press Reset to return to original values')?>
+  </td>
+</tr>
+      <tr>
+	 <td class='cell' colspan='4'>
+	 <input type='submit' value='<?=uptr('More')?>' onClick="return setTodo('MORE')">
+         &nbsp;
+         <input type='submit' value='<?=uptr('less')?>' onClick="return setTodo('LESS')">
+	 &nbsp;
+	 <input type='submit' class='submit' value='<?=tr('Save')?>' onClick="return setTodo('Save')">
+	 &nbsp;
+	 <input type='submit' class='submitPlain' value='<?=tr('Reset')?>' onClick="return setTodo('Reset')">
+         </td>
+      </tr>
+      </table>
+      </ul>
+
+      <input type='hidden' name='more' value='<?=$more?>'>
+      </form>
+      <script language='javascript'>
+	 function setTodo( val ) {
+	   document.behaviorDetailForm.TODO.value = val;
+	 } 	 
+      </script> 
+
---
+++ www/admin/editBehaviorDetails.php
@@ -0,0 +1,84 @@
+<?
+  /*
+  **  EDIT BEHAVIOR DETAILS
+  **  
+  **  Modifies the details of an existing behavior
+  **
+  */
+  
+  
+  include("admin_header.php");
+
+  if( $TODO == 'MORE' ) {
+    $more = $more+3;
+  } else if( $TODO == 'LESS' ) {
+    $more = 0;
+  }
+
+  $elements=array();
+
+//When the action is MORE or LESS, we fill the elements array with the values from the Form
+  if( $TODO == 'MORE' || $TODO == 'LESS' ) {
+    for( $i=0; $i<count($NewFieldName); $i++ ) {
+      if( $NewFieldName[$i] && strlen($NewFieldName[$i])>0 ) {
+        $elements[] = array(
+                        "field_name" => $NewFieldName[$i],
+                        "comparator" => $NewComparator[$i],
+                        "match_value"=> $NewMatchValue[$i],
+                        "sort_order" => $NewSortOrder[$i]
+                        );
+      }
+    }
+  } else if( $TODO == 'Save' ) {
+//When the action is Save and demo_mode is on, we don't save the changes to the database
+    if( $zen->demo_mode == "on" ) {
+      $msg = tr("Process completed successfully. Behaviors were not updated since this is a demo site.");
+      $skip = 1;
+    } else {
+      $j = 0;
+      for( $i=0; $i<count($NewFieldName); $i++ ) {
+        if( $NewFieldName[$i] && strlen($NewFieldName[$i])>0 ) {
+          $elements[] = array(
+                        "field_name" => $NewFieldName[$i],
+                        "comparator" => $NewComparator[$i],
+                        "match_value"=> $NewMatchValue[$i],
+                        "sort_order" => $NewSortOrder[$i]
+                              );
+          $j++;
+        }
+      }
+//Last commands for the Save action when demo_mode is not on:
+      $zen->updateBehaviorDetails($behavior_id, $elements) ;
+      $msg = tr("? elements were saved in the selected behavior. Updates complete", array($j));
+      $skip = 1;
+    }
+  } else {
+    $behavior         = $zen->getBehaviorList( array($behavior_id) );
+    $behavior_details = $behavior[$behavior_id]['fields'];
+    if ( !is_array($behavior_details) ) {
+      $behavior_details=array();
+    }
+    $TODO = 'EDIT';
+    foreach ($behavior_details as $v) {
+      if ( $v['field_name'] && strlen($v['field_name'])>0 ) {
+        $elements[]=$v;
+      }
+    }
+  }
+  $page_title = ($skip)? tr("Admin Section") : tr("Edit Behavior Details");
+
+  $field_list=$zen->getBehaviorDestinationFieldsArray();
+  $comp_opers=$zen->getBehaviorOperators();
+
+  include("$libDir/nav.php");
+  $zen->printErrors($errs);
+
+  if( !$skip ) {
+    include("$templateDir/behaviorDetailForm.php");
+  } else {
+    include("$templateDir/adminMenu.php");
+  }
+  include("$libDir/footer.php");
+
+
+?>
