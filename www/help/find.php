52e2396a4331d75b3f681d35b888c2a622a3a3d7
phpzen <phpzen>
2004-09-15 18:50:41 +0000

    fix null problems with custom groups

--- www/help/find.php
+++ www/help/find.php
@@ -4,5 +4,10 @@
   include("$b/help_header.php");
   $s = $zen->checkAlphaNum($_GET['s']);
   $p = $zen->checkAlphaNum($_GET['p']);
-  include("$helpDir/$s/$p.php");
+  if( @file_exists("$helpDir/$s/$p.php") ) {
+    include("$helpDir/$s/$p.php");
+  }
+  else {
+    print "<b>Sorry, not found</b>\n"; 
+  }
 ?>
