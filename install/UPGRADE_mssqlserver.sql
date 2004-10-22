84dc4de1656317c54d020d21a3009e375425c667
cfalonso <cfalonso>
2004-10-22 22:30:57 +0000

    Add admin settings to keep owners when moving a ticket, closing it and setting it to pending

--- install/UPGRADE_mssqlserver.sql
+++ install/UPGRADE_mssqlserver.sql
@@ -217,6 +217,11 @@ INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (98,
 INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (99,'allow_contacts', 'on', 'Specify whether contacts will be used or not');
 INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (100,'level_contacts','3','Level required to view the contacts');
 INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (101,'paging_max_rows','20','Number of rows to display at a time');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (102,'retain_owner_move','on','Keep owner data on tickets after a ticket is moved between bins');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (103,'retain_owner_pending','on','Keep owner data on tickets after
+ a ticket is set to PENDING');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (104,'retain_owner_closed','on','Keep owner data on tickets after a ticket is set to CLOSED');
+
 
 -- CREATE AN ENTRY FOR EACH EXISTING TICKET IN THE VARFIELDS TABLE
 INSERT INTO ZENTRACK_VARFIELD (ticket_id) SELECT id FROM ZENTRACK_TICKETS;
