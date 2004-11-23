2f5c31bd052a2dd83e5c335e04c6fcdab66e4e54
cfalonso <cfalonso>
2004-11-23 18:34:15 +0000

    Fixed call to tr function

--- www/actions/contact_edit.php
+++ www/actions/contact_edit.php
@@ -3,7 +3,7 @@
   **  Action: edit ticket
   */
   include_once("../header.php");
-  $page_title = tr("Contact #?", $id);
+  $page_title = tr("Contact #?", array($id));
   $expand_contact = 1;
   
   
