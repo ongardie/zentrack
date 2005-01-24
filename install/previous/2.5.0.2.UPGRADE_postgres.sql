f8e4688bc91e25cb28339266a2d8e817e6f8cc85
phpzen <phpzen>
2005-01-24 04:27:49 +0000

    added upgrade files

---
+++ install/previous/2.5.0.2.UPGRADE_postgres.sql
@@ -0,0 +1,10 @@
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (102,'retain_owner_move','on','Keep owner data on tickets after a ticket is moved between bins');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (103,'retain_owner_pending','on','Keep owner data on tickets after a ticket is set to PENDING');
+
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (104,'retain_owner_closed','on','Keep owner data on tickets after a ticket is set to CLOSED');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (105,'character_set','ISO-8859-15','Character set to be used');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (106,'default_start_date_hours','on','Include hours in default start date for new tickets');
+INSERT INTO ZENTRACK_SETTINGS (setting_id, name, value, description) VALUES (107,'default_deadline_hours','on','Include hours in default deadline for new tickets');
+
+-- Modify the version number
+UPDATE ZENTRACK_SETTINGS SET value='2.5.0.2' WHERE setting_id=74;
