7e7e57597f1a5b271c7a9cc82a38d1082225d002
phpzen <phpzen>
2006-03-12 08:16:05 +0000

    Install files updated for release

---
+++ install/UPGRADE_oracle.sql
@@ -0,0 +1,13 @@
+
+-- Modify the version number
+UPDATE ZENTRACK_SETTINGS SET value='2.6.1' WHERE setting_id=74;
+
+INSERT INTO ZENTRACK_SETTINGS ( setting_id , name , value, description ) VALUES (121, 'max_textbox_height', '200', 'Max height of textbox in ticket view');
+
+-- new color settings
+ALTER TABLE ZENTRACK_PRIORITIES ADD color varchar2(25) default '';
+UPDATE ZENTRACK_PRIORITIES SET color = '#FFFFFF' where name = 'None';
+UPDATE ZENTRACK_PRIORITIES SET color = '#FFFFCC' where name = 'Low';
+UPDATE ZENTRACK_PRIORITIES SET color = '#FFDDBB' where name = 'Medium';
+UPDATE ZENTRACK_PRIORITIES SET color = '#FFBBAA' where name = 'High';
+UPDATE ZENTRACK_PRIORITIES SET color = '#FF9999' where name = 'Critical';
